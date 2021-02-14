32c4b9b4f7ccba916c4c8406b5533446f7dbad1d
Sanjay Singh Bisht <bishtsanjay343@gmail.com>
2021-02-14 13:13:01 +0530

    Added changes to testing

--- components/Game/__tests__/Game.test.js
+++ components/Game/__tests__/Game.test.js
@@ -1,9 +1,12 @@
-import Game from "..";
+import Game from "../Game";
 import { render } from '@testing-library/react';
+import MyApp from '../../../pages/_app';
 import renderer from 'react-test-renderer';
 
 test('Renders Components Nicely', () => {
-    const tree = renderer.create(<Game></Game>).toJSON();
+    const tree = renderer.create(<MyApp>
+      <Game></Game>
+      </MyApp>).toJSON();
     expect(tree).toMatchSnapshot();
   })
   
---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+    setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+    moduleFileExtensions: [
+        "js",
+        "jsx"
+      ],
+    moduleNameMapper: {
+        "\\.(css|less)$": "identity-obj-proxy"
+      },
+    transform: {
+      "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+      "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+    }
+  };
